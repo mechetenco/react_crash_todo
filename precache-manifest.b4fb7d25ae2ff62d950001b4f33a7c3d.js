0ec29c001a7a295f3e980863bde5a775fd525872
Nwabueze Amaechi Christian <nwabuezeamaechichristian@gmail.com>
2020-06-24 15:37:33 +0100

    Updates

---
+++ precache-manifest.b4fb7d25ae2ff62d950001b4f33a7c3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa5b0c9fd1ae7308a66d2956834274b2",
+    "url": "/react_crash_todo/index.html"
+  },
+  {
+    "revision": "a87cf90fc2b0aa301d91",
+    "url": "/react_crash_todo/static/css/main.f56cc26d.chunk.css"
+  },
+  {
+    "revision": "b8fdc5a9c5664e786c46",
+    "url": "/react_crash_todo/static/js/2.d7a6dafc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react_crash_todo/static/js/2.d7a6dafc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a87cf90fc2b0aa301d91",
+    "url": "/react_crash_todo/static/js/main.9cfeb84a.chunk.js"
+  },
+  {
+    "revision": "a5841ee4df33ef9ddec4",
+    "url": "/react_crash_todo/static/js/runtime-main.a65190b8.js"
+  }
+]);
